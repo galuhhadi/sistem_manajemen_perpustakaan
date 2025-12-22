ce7432f5c2a34178df6a4d508773e14cc690b354
galuhhad <galuhfurqon25@gmail.com>
2025-12-23 00:50:48 +0800

    Setup skema tabel borrowings dengan foreign keys

--- database/migrations/2025_12_22_164519_create_borrowings_table.php
+++ database/migrations/2025_12_22_164519_create_borrowings_table.php
@@ -9,13 +9,22 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('borrowings', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   public function up()
+{
+    Schema::create('borrowings', function (Blueprint $table) {
+        $table->id();
+
+        // Relasi ke Users & Books (Cascade: Hapus peminjaman jika user/buku dihapus)
+        $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+        $table->foreignId('book_id')->constrained('books')->cascadeOnDelete();
+
+        $table->date('borrow_date');
+        $table->date('due_date'); // Batas waktu kembali
+        $table->date('return_date')->nullable(); // Diisi saat dikembalikan
+        $table->enum('status', ['borrowed', 'returned'])->default('borrowed');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
